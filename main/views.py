478bae3ae4d8662f00af0f5d05e6a212a87110ef
BuckytheBoss <buchman.avi@gmail.com>
2021-08-17 11:48:17 +0300

    changed a few shortcuts

--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
 from .models import Vehicle, Person
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 
 # Create your views here.
 
 def index(request):
-    cars = Vehicle.objects.all()
+    cars = get_list_or_404(Vehicle, color='red')
     return render(request, 'index.html', {'cars':cars})
 
 def person_view(request,person_id):
-    p = Person.objects.get(id=person_id)
+
+    # p = Person.objects.get(id=person_id)
+    p = get_object_or_404(Person, id=person_id)
+
     return render(request, 'person.html', {'person':p})
     
